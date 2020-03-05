8e0c188c006a3007848ddfef8dc6c37ff121bab2
JeffRThomas <thomas.jeff@live.com>
2020-03-05 14:58:18 -0600

    Final Push

--- Jeff-MaryCW.js
+++ Jeff-MaryCW.js
@@ -1,22 +1,27 @@
 console.log("Team JS Linked");
 // Get reference variables for elements & verify selections
-let userName = document.getElementById("");
+let userName = document.getElementById("nameInput");
 console.log(userName);
-let userEmail = document.getElementById("");
+let userEmail = document.getElementById("emailInput");
 console.log(userEmail);
-let userMessage = document.getElementById("");
+let userMessage = document.getElementById("messageInput");
 console.log(userMessage);
-let userSubmitButton = document.getElementById("");
+let userSubmitButton = document.getElementById("button");
 console.log(userSubmitButton);
+let userStoredData = { YourName: "", YourEmail: "", Message: "", };
 // *******************************************************************************************
 // Function Declaration to Alert User of Information Submitted
 let sendInfo = (userclick) => {
-    preventDefault;
-    let userStoredData= [`
-        Your Name:${userName.value} Your Email:${userEmail.value} Message:${userMessage.value}` 
-    ];
+    console.log("Function Called");
     alert(`Your Name: ${userName.value}\nYour Email: ${userEmail.value}\nMessage: ${userMessage.value}`);
-    console.log(`${userName.value}\n${userEmail.value}\n${userMessage.value}`);
-    console.log(userStoredData);
+    class userStoredData {
+        constructor(userName, userEmail, userMessage) {
+            this.userName = userName;
+            this.userEmail = userEmail;
+            this.userMessage = userMessage;
+        }
+    }
 }
+new userStoredData1(userName, userEmail, userMessage);
+console.log(`Your Name:${this.userName}\n:Your Email:${this.userEmail}\nMessage:${this.userMessage}`);
 userSubmitButton.addEventListener("click", sendInfo);
